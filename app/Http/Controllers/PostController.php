74f1660e6212da678514850e10fcd812b0428d72
Paolo Colombo <ssaffru@gmail.com>
2021-06-04 22:28:49 +0200

    removed print

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -41,9 +41,6 @@ class PostController extends Controller
     {
         $products = unserialize($request->products);
 
-        print("this is a test");
-        print($request);
-
         [$result, $tot_taxes, $products] = CommonController::calculate($products);
 
         return view('result')->with('result', $result)->with('tot_taxes', $tot_taxes)->with('products', $products);
